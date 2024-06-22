9abd77493f3930b3e4322e73c19c7a36ef7a81fc
Chris Salvador <csalvador58@gmail.com>
2024-06-21 23:37:21 -0700

    Added alert handlers

---
+++ Appetizers/Alert.swift
@@ -0,0 +1,33 @@
+//
+//  Alert.swift
+//  Appetizers
+//
+//  Created by Salvador on 6/21/24.
+//
+
+import SwiftUI
+
+struct AlertItem: Identifiable {
+    let id = UUID()
+    let title: Text
+    let message: Text
+    let dismissButton: Alert.Button
+}
+
+struct AlertContext {
+    static let invalidData = AlertItem(title: Text("Server Error"),
+                                       message: Text("The data received from the server is invalid. Please contant support."),
+                                       dismissButton: .default(Text("Ok")))
+    
+    static let invalidResponse = AlertItem(title: Text("Server Error"),
+                                           message: Text("Invalid response from the server. Please try again later or contact support."),
+                                           dismissButton: .default(Text("Ok")))
+    
+    static let invalidURL = AlertItem(title: Text("Server Error"),
+                                      message: Text("There was an issue connecting to the server. If this persists, please contact support."),
+                                      dismissButton: .default(Text("Ok")))
+    
+    static let unableToComplete = AlertItem(title: Text("Server Error"),
+                                            message: Text("Unable to complete your request at this time. Please check your network connection."),
+                                            dismissButton: .default(Text("Ok")))
+}
---
+++ Appetizers/AppetizerListViewModel.swift
@@ -0,0 +1,44 @@
+//
+//  AppetizerViewModel.swift
+//  Appetizers
+//
+//  Created by Salvador on 6/21/24.
+//
+
+import SwiftUI
+
+@Observable class AppetizerListViewModel {
+    // An array to hold the fetched appetizers, initially empty
+    var appetizers: [Appetizer] = []
+    var alertItem: AlertItem?
+    
+    // Function to fetch appetizers from the network
+    func getAppetizers() {
+        // Call the NetworkManager's getAppetizers function
+        NetworkManager.shared.getAppetizers { [self] result in
+            // Ensure UI updates are performed on the main thread
+            DispatchQueue.main.async { [self] in
+                // Handle the result of the network request
+                switch result {
+                case .success(let appetizers):
+                    // If successful, update the appetizers array with fetched data
+                    self.appetizers = appetizers
+                case .failure(let error):
+                    switch error {
+                    case .invalidResponse:
+                        alertItem = AlertContext.invalidResponse
+                        
+                    case .invalidURL:
+                        alertItem = AlertContext.invalidURL
+                        
+                    case .invalidData:
+                        alertItem = AlertContext.invalidData
+                        
+                    case .unableToComplete:
+                        alertItem = AlertContext.unableToComplete
+                    }
+                }
+            }
+        }
+    }
+}
