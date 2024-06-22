6abc3637fc2f3d169d4eba077704379134db0651
Chris Salvador <csalvador58@gmail.com>
2024-06-21 17:42:26 -0700

    Setup model

---
+++ Appetizers/AppetizerListView.swift
@@ -0,0 +1,22 @@
+//
+//  AppetizerListView.swift
+//  Appetizers
+//
+//  Created by Salvador on 6/21/24.
+//
+
+import SwiftUI
+
+struct AppetizerListView: View {
+    var body: some View {
+        NavigationView {
+            Text("Appetizer List View")
+                .navigationTitle("🍟 Appetizers")
+        }
+        
+    }
+}
+
+#Preview {
+    AppetizerListView()
+}
---
+++ Appetizers/OrderView.swift
@@ -0,0 +1,21 @@
+//
+//  OrderView.swift
+//  Appetizers
+//
+//  Created by Salvador on 6/21/24.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+    var body: some View {
+        NavigationView {
+            Text("Orders")
+                .navigationTitle("🏧 Orders")
+        }
+    }
+}
+
+#Preview {
+    OrderView()
+}
